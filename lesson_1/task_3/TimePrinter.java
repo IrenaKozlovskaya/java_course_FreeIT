743bba3c30ebf9370d73a650826977b14b3ce02a
Irena Kozlovskaya <irena.kozlovskaya11@gmail.com>
2021-02-17 18:08:09 +0300

    Homework

---
+++ lesson_1/task_3/TimePrinter.java
@@ -0,0 +1,17 @@
+public class TimePrinter {
+    public static void main (String [] arg){
+        int s = 4500;
+        int sec = 4500 % 60;
+        int m = (s - sec) / 60;
+        int min = m % 60;
+        int h = (m - min) / 60;
+        int hour = h % 24;
+        int d = (h - hour) / 24;
+        int day = d % 7;
+        int week = (d - day) / 7;
+        System.out.println("A)"+ m + " min, " + sec + " sec");
+        System.out.println("B)"+ h + " hours, " + min + " min, " + sec + " sec");
+        System.out.println("C)"+ d + " days, " + hour + " hours, " + min + " min, " + sec + " sec");
+        System.out.println("D)"+ week + " weeks, " + day + " days, " + hour + " hours, " + min + " min, " + sec + " sec");
+    }
+}
---
+++ lesson_1/task_5/DifferentNumbers.java
@@ -0,0 +1,24 @@
+public class DifferentNumbers {
+    public static void main(String[] args) {
+        int n = 2516;
+        String s = String.valueOf(n);
+        int count = 0;
+        if (s.length() == 4) {
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = 0; j < s.length(); j++) {
+                    if (s.charAt(i) == s.charAt(j) && j != i){
+                        count++;
+                    }
+                }
+            }
+            if (count > 0) {
+                System.out.print("False");
+            } else if (count == 0) {
+                System.out.print("True");
+            }
+        }
+        else {
+            System.out.print("Entered the wrong number");
+        }
+    }
+}
